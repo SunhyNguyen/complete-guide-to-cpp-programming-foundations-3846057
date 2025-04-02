f6a9f289fa1b8e8e68c0820d0dd34e22f74d6bcb
Your Name <you@example.com>
2025-04-02 08:41:08 -0400

    I push everything

--- src/Ch02/02_08b/CodeDemo_3.cpp
+++ src/Ch02/02_08b/CodeDemo_3.cpp
@@ -6,11 +6,11 @@ enum class asset_type {texture, sound, animation, script};
 enum class menu_section {background, music, sound, controls, texture};
 
 int main(){
-    int asset_value;
+    asset_type asset_value;
 
-    asset_value = (int)asset_type::sound;
+    asset_value = asset_type::sound;
 
-    std::cout << "asset_value = " << asset_value << std::endl;
+    std::cout << "asset_value = " << (int)asset_value << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
