7d3edb0e1283baf8141aa2b42b6e10a7e03f5f79
SunhyNguyen <anhnguyen273@usf.edu>
2025-04-02 12:09:49 +0000

    I created 2 more demo file in chapter 2 part 8 so I can practice

---
+++ src/Ch02/02_08b/CodeDemo_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdint>
+
+enum asset_type {texture, sound, animation, script};
+
+int main(){
+    int asset_value;
+
+    asset_value = sound;
+
+    std::cout << "asset_value = " << asset_value << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
